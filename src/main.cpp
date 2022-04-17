d40559eb78e318e7396427ed9216882b085bb6d4
Paul Hayler <paul.hayler@outlook.com>
2022-04-17 12:35:06 +0100

    Lint corrections

--- src/main.cpp
+++ src/main.cpp
@@ -677,7 +677,7 @@ void updateSeg() {
   }
 
   // Go to the next segment
-  if (segPhase == 1 && millis() - holdStart >= segHold[segNum - 1] * 60000) {
+  if ((segPhase == 1) && (millis() - holdStart) >= (segHold[segNum - 1] * 60000)) {
     segNum = segNum + 1;
     segPhase = 0;
     rampStart = millis();
